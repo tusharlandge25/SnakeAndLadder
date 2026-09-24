5e988c5125e79f36fcf06e07dd77b4fb4854b3a1
Tushar <tusharl@host.docker.internal>
2026-09-24 14:10:21 +0530

    Code changes regarding player positions after snake and ladder.

--- src/mypack/AnyNumberOfPlayers.java
+++ src/mypack/AnyNumberOfPlayers.java
@@ -9,8 +9,7 @@ public class AnyNumberOfPlayers {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Start");
-		System.out.print("Enter number of Players comes to play : ");
+		System.out.print("Enter number of Players : ");
 		Scanner sc = new Scanner(System.in);
 		int numberOfPlayers = sc.nextInt();
 		
@@ -27,15 +26,22 @@ public class AnyNumberOfPlayers {
 		
 		int endPosition = Game.START;
 		boolean winnerFound = false;
-		
+		sc.nextLine();
+		System.out.println();
 		while (endPosition < Game.END) {
 			System.out.println("end position : " + endPosition);
+			System.out.println();
 			for (int i = 0; i < list.size(); i++) {
-				int randomNum = (int) (Math.random() * 6) + 1;
-				System.out.println("Player" + (i + 1) + " is playing and got " + randomNum);
+				System.out.println("Player" + (i + 1) + "'s turn ");
+				System.out.println("Press ENTER to Roll the dice");
+				int randomNum = 0;
+				if(sc.nextLine() != null) {
+					randomNum = (int) (Math.random() * 6) + 1;
+				}
+				System.out.println("Player" + (i + 1) + " got " + randomNum);
 				list.get(i).setCurPos(randomNum + list.get(i).getCurPos());
 				System.out.println("Player" + (i + 1) + " position : " + list.get(i).getCurPos());
-				
+				System.out.println();
 				if(list.get(i).getCurPos() >= Game.END) {
 					System.out.println();
 					System.out.println("Player" + (i + 1) + " Won.");
@@ -44,15 +50,15 @@ public class AnyNumberOfPlayers {
 				}
 				
 				if (snakePositions.contains(list.get(i).getCurPos())) {
+					System.out.println("Snake Bite");
 					list.get(i).setCurPos(list.get(i).getCurPos() - mapForSnake.get(list.get(i).getCurPos()));
 					System.out.println("Player" + (i + 1) + " position after snake : " + list.get(i).getCurPos());
-					break;
 				}
 				
 				if (ladderPositions.contains(list.get(i).getCurPos())) {
+					System.out.println("Got the ladder");
 					list.get(i).setCurPos(list.get(i).getCurPos() + mapForLadder.get(list.get(i).getCurPos()));
-					System.out.println("Player" + (i + 1) + " position after snake : " + list.get(i).getCurPos());
-					break;
+					System.out.println("Player" + (i + 1) + " position after ladder : " + list.get(i).getCurPos());
 				}
 			}
 			if(winnerFound) break;
